50c35c5d0e9d6982fe71dff4b7f3749398da7627
Giorgio <giorgioatanasov34@yahoo.com>
2020-06-14 02:10:51 -0700

    Updates

---
+++ precache-manifest.50180f790b37b7a6160cb35e2c83f106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1aee82f70b46b44acff12b732d378b6",
+    "url": "/websitev2/index.html"
+  },
+  {
+    "revision": "bb99f2f1283ab5fc8276",
+    "url": "/websitev2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0c3bb3f1d5e80a7a3c12",
+    "url": "/websitev2/static/js/2.6c230430.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/websitev2/static/js/2.6c230430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb99f2f1283ab5fc8276",
+    "url": "/websitev2/static/js/main.0d97ebb9.chunk.js"
+  },
+  {
+    "revision": "b9399592c59335b8654a",
+    "url": "/websitev2/static/js/runtime-main.c8b76214.js"
+  }
+]);
